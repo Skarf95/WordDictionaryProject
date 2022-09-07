38d48b70b62d02fb3a2aaee5a6884c61ca8ad2a2
euninjun <honestjun@live.com>
2022-09-07 10:43:30 +0900

    1st commit

---
+++ WordDictionaryProject/src/com/myword/com/Main.java
@@ -0,0 +1,14 @@
+ package com.myword.com;
+
+import com.myword.com.WordManager;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new WordManager( ).start( );
+		
+
+	}
+
+}
